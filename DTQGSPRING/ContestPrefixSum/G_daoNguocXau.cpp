9bb45e141fb44c7656bd6655bfac8a0b5aacf3c4
whlongg <nhl08.contact@gmail.com>
2025-03-17 00:14:20 +0700

    Clean up G_daoNguocXau.cpp by removing commented-out code and unnecessary sections

--- DTQGSPRING/ContestPrefixSum/G_daoNguocXau.cpp
+++ DTQGSPRING/ContestPrefixSum/G_daoNguocXau.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -23,30 +22,4 @@ int main()
             swap(s[i - 1], s[n - i]);
     cout << s << '\n';
     return 0;
-=======
-#include <bits/stdc++.h>
-using namespace std;
-
-int main()
-{
-    ios_base::sync_with_stdio(0);cin.tie(0);
-    string s;
-    cin >> s;
-    int n = s.length();
-    int q;
-    cin >> q;
-    vector<int> k(q);
-    for (int i = 0; i < q; ++i)
-        cin >> k[i];
-    vector<bool> flip(n / 2 + 1, false);
-    for (int i = 0; i < q; i++)
-        flip[k[i]] = !flip[k[i]];
-    for (int i = 1; i <= n / 2; i++)
-        flip[i] = flip[i] ^ flip[i - 1];
-    for (int i = 1; i <= n / 2; i++)
-        if (flip[i])
-            swap(s[i - 1], s[n - i]);
-    cout << s << '\n';
-    return 0;
->>>>>>> 5c268531b0287f0d2fe68db2110832e6f8451896
 }
