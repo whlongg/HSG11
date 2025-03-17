a55f24a689e90d89b38d82dc143037b57162ea8c
whlongg <nhl08.contact@gmail.com>
2025-03-18 01:21:48 +0700

    Add new test2.cpp file and update VSCode configuration for g++-14 build

---
+++ test2.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int32_t main(){
+	int n; cin >> n; 
+	map <int, int> mp;
+	for(int i = 1; i <= n; ++i){
+		cin >> mp[i];
+	}
+	for(auto x: mp)
+		cout << 
+}
